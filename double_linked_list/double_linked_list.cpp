06909afbb6f43016c2e91cef106668d972727f69
ImamDwiYanPermana <imampermana224@gmail.com>
2023-05-26 16:09:53 +0700

    Membuat Proses START samad engan null dan start nomhs

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -33,7 +33,22 @@ void doubleleLinkedList::addNode() {
 
     Node* newNode = new Node(); // step1
     newNode->noMhs = nim;//step 2
-    newNode->name = nm;//step 2
+    newNode->name = nm;//step 2}
+
+    /*insert a node in teh beging of  a doubly - linked list*/
+    if (START == NULL || nim <= START->noMhs) { // chek if data null
+        if (START != NULL && nim == START->noMhs) {
+            cout << "\n duplicate not allowed" << endl;
+            return;
+        }
+        newNode->next = START; // STep 3
+        if (START != NULL) 
+            START->prev = newNode; // Step 4
+        newNode->prev = NULL; // Step 5
+        START = newNode; /// Step 6
+        return;
+    }
+
 }
 
 int main()
