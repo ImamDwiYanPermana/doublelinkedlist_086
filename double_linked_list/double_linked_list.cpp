479f010d686939f86302961cedf91ce6085e0209
ImamDwiYanPermana <imampermana224@gmail.com>
2023-05-26 20:20:46 +0700

    menambahkan fungsi travse untnk mencetak cataatn dengan urutan

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -90,6 +90,18 @@ bool doubleleLinkedList::deletNode(int rollNo) {
 bool doubleleLinkedList::listEmpty() {
     return (START == NULL);
 }
+void doubleleLinkedList::traverse(){
+    if (listEmpty())
+        cout << "\nList is empty" << endl;
+    else {
+        cout << "\nRecords in ascending order of roll number are: " << endl;
+        Node* currentNode = START;
+        while (currentNode != NULL) {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->next;
+        }
+    }
+}
 int main()
 {
     std::cout << "Hello World!\n";
